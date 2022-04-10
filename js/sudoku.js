4e90346abf9f4239ee414dc53f586dae52ded990
Arunima Chaudhuri <62856848+tinniaru3005@users.noreply.github.com>
2022-04-10 16:43:03 +0530

    Update sudoku.js

--- js/sudoku.js
+++ js/sudoku.js
@@ -2,13 +2,101 @@
 
 // Board is List[List[Int]] or false
 // interp. as a sudoku board, or false if the given board is invalid
-// EXAMPLE BOARDS
+
+function clearInputBoard() {
+    document.getElementById("1").value = "";
+    document.getElementById("2").value = "";
+    document.getElementById("3").value = "";
+    document.getElementById("4").value = "";
+    document.getElementById("5").value = "";
+    document.getElementById("6").value = "";
+    document.getElementById("7").value = "";
+    document.getElementById("8").value = "";
+    document.getElementById("9").value = "";
+
+    document.getElementById("10").value = "";
+    document.getElementById("11").value = "";
+    document.getElementById("12").value = "";
+    document.getElementById("13").value = "";
+    document.getElementById("14").value = "";
+    document.getElementById("15").value = "";
+    document.getElementById("16").value = "";
+    document.getElementById("17").value = "";
+    document.getElementById("18").value = "";
+
+    document.getElementById("19").value = "";
+    document.getElementById("20").value = "";
+    document.getElementById("21").value = "";
+    document.getElementById("22").value = "";
+    document.getElementById("23").value = "";
+    document.getElementById("24").value = "";
+    document.getElementById("25").value = "";
+    document.getElementById("26").value = "";
+    document.getElementById("27").value = "";
+
+    document.getElementById("28").value = "";
+    document.getElementById("29").value = "";
+    document.getElementById("30").value = "";
+    document.getElementById("31").value = "";
+    document.getElementById("32").value = "";
+    document.getElementById("33").value = "";
+    document.getElementById("34").value = "";
+    document.getElementById("35").value = "";
+    document.getElementById("36").value = "";
+
+    document.getElementById("37").value = "";
+    document.getElementById("38").value = "";
+    document.getElementById("39").value = "";
+    document.getElementById("40").value = "";
+    document.getElementById("41").value = "";
+    document.getElementById("42").value = "";
+    document.getElementById("43").value = "";
+    document.getElementById("44").value = "";
+    document.getElementById("45").value = "";
+
+    document.getElementById("46").value = "";
+    document.getElementById("47").value = "";
+    document.getElementById("48").value = "";
+    document.getElementById("49").value = "";
+    document.getElementById("50").value = "";
+    document.getElementById("51").value = "";
+    document.getElementById("52").value = "";
+    document.getElementById("53").value = "";
+    document.getElementById("54").value = "";
+
+    document.getElementById("55").value = "";
+    document.getElementById("56").value = "";
+    document.getElementById("57").value = "";
+    document.getElementById("58").value = "";
+    document.getElementById("59").value = "";
+    document.getElementById("60").value = "";
+    document.getElementById("61").value = "";
+    document.getElementById("62").value = "";
+    document.getElementById("63").value = "";
+
+    document.getElementById("64").value = "";
+    document.getElementById("65").value = "";
+    document.getElementById("66").value = "";
+    document.getElementById("67").value = "";
+    document.getElementById("68").value = "";
+    document.getElementById("69").value = "";
+    document.getElementById("70").value = "";
+    document.getElementById("71").value = "";
+    document.getElementById("72").value = "";
+
+    document.getElementById("73").value = "";
+    document.getElementById("74").value = "";
+    document.getElementById("75").value = "";
+    document.getElementById("76").value = "";
+    document.getElementById("77").value = "";
+    document.getElementById("78").value = "";
+    document.getElementById("79").value = "";
+    document.getElementById("80").value = "";
+    document.getElementById("81").value = "";
+}
+
 
 const b = null
-
-
-
-
 
 // // ____FUNCTION DEFINITIONS____ // //
 
